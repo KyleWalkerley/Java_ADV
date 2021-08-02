39121914b1022d5a747e3404bf9c38d065547270
KyleWalkerley <walkerleykyle@gmail.com>
2021-08-02 15:17:27 +0200

    02/08/2021

--- JavaExcersises/src/main/java/Employee/EmployeeTest.java
+++ JavaExcersises/src/main/java/Employee/EmployeeTest.java
@@ -12,15 +12,15 @@ import com.example.domain.Employee;
  * @author kyle_
  */
 public class EmployeeTest {
-    public static void main(String[] args) {
-        Employee emp = new Employee ();
-        emp.setEmpId(101);
-        emp.setName("Kyle Walkerley");
-        emp.setSsn ("021-55-4201");
-        emp.setSalary(185_000.00);
-        System.out.println ("Employee id:         " + emp.getEmpId());
-        System.out.println ("Employee name:       " + emp.getName());
-        System.out.println ("Employee Soc Sec #:  " + emp.getSsn());
-        System.out.println ("Employee salary:     " + emp.getSalary());
-    }
+//    public static void main(String[] args) {
+//        Employee emp = new Employee ();
+//        emp.setEmpId(101);
+//        emp.setName("Kyle Walkerley");
+//        emp.setSsn ("021-55-4201");
+//        emp.setSalary(185_000.00);
+//        System.out.println ("Employee id:         " + emp.getEmpId());
+//        System.out.println ("Employee name:       " + emp.getName());
+//        System.out.println ("Employee Soc Sec #:  " + emp.getSsn());
+//        System.out.println ("Employee salary:     " + emp.getSalary());
+//    }
 }
