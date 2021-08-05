a96c611a735f6bd19ce0c209fe60fba51b0fbb3f
KyleWalkerley <walkerleykyle@gmail.com>
2021-08-05 08:55:42 +0200

    04/08/2021

---
+++ JAVA11_ADV_CLASSWORK/src/main/java/Lessons10/Gender.java
@@ -0,0 +1,6 @@
+package Lessons10;
+
+/**
+ * @author MikeW
+ */
+public enum Gender { MALE, FEMALE }
---
+++ JAVA11_ADV_CLASSWORK/src/main/java/Lessons10/Role.java
@@ -0,0 +1,6 @@
+package Lessons10;
+
+/**
+ * @author MikeW
+ */
+public enum Role { STAFF, MANAGER, EXECUTIVE }
