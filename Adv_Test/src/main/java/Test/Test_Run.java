adb0d0df8b5d49c6d2847529b91061299142b2b7
KyleWalkerley <walkerleykyle@gmail.com>
2021-08-06 08:51:57 +0200

    05/08/2021

---
+++ Adv_Test/src/main/java/Test/Test_Run.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Test;
+
+/**
+ *
+ * @author kyle_
+ */
+public class Test_Run {
+//   public boolean valid = "false";
+//   public String s = "A new string";
+//   public int i = 40_000.00;
+//   public BrokenClass(){
+//       
+//   }
+    
+//    public static void main(String[] args) {
+//        String s1 = new String("Test");
+//        String s2 = new String("Test");
+//        
+//        if (s1==s2)
+//            System.out.println("Same");
+//        if (s1.equals(s2))
+//            System.out.println("Equals");
+//    }
+    
+    
+}
