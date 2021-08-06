7d8b992ac5960421e2be5b3dcab01e3b31c4ada2
KyleWalkerley <walkerleykyle@gmail.com>
2021-08-06 11:34:23 +0200

    06/08/2021

--- JavaADV_Excersises/src/main/java/Excersise6_1/PetMain.java
+++ JavaADV_Excersises/src/main/java/Excersise6_1/PetMain.java
@@ -12,17 +12,41 @@ package Excersise6_1;
 public class PetMain {
     public static void main(String[] args) {
         Animal a;
-        //test a spider with a spider reference
         Spider s = new Spider();
         s.eat();
         s.walk();
-        //test a spider with an animal reference
         a = new Spider();
         a.eat();
         a.walk();
+
+        Pet p;
+
+        Cat c = new Cat("Tom");
+        c.eat();
+        c.walk();
+        c.play();
+        p = new Cat();
+        p.setName("Mr. Whiskers");
+        p.play();
+
+        Fish f = new Fish();
+        f.setName("Guppy");
+        f.eat();
+        f.walk();
+        f.play();
+
+
+        playWithAnimal(s);
+        playWithAnimal(c);
+        playWithAnimal(f);
     }
-    
+
     public static void playWithAnimal(Animal a) {
-        // completed in practice
+        if (a instanceof Pet) {
+            Pet p = (Pet) a;
+            p.play();
+        } else {
+            System.out.println("Danger! Wild Animal");
+        }
     }
 }
