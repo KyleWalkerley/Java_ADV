ef2555a9f7a993c5b944395002e7ceb2c16281ba
KyleWalkerley <walkerleykyle@gmail.com>
2021-08-16 09:09:34 +0200

    16/08/2021

---
+++ JAVA11_ADV_CLASSWORK/src/main/java/Lessons18/SimpleJDBCTest.java
@@ -0,0 +1,45 @@
+package Lessons18;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Date;
+
+public class SimpleJDBCTest {
+
+    public static void main(String[] args) {
+        String url = "jdbc:postgresql://localhost:5432/postgresql";
+        String username = "posgre";
+        String password = "Stupid@1";
+        String query = "SELECT * FROM employeesql";
+        try (Connection con = DriverManager.getConnection(url, username, password);
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(query)){
+            while (rs.next()) {
+                int empID = rs.getInt("ID");
+                String first = rs.getString("FIRSTNAME");
+                String last = rs.getString("LASTNAME");
+                Date birthDate = rs.getDate("BIRTHDATE");
+                double salary = rs.getDouble("SALARY");
+                System.out.println("Employee ID:   " + empID + "\n"
+                        + "Employee Name: " + first + " " + last + "\n"
+                        + "Birth Date:    " + birthDate + "\n"
+                        + "Salary:        " + salary + "\n");
+            } // end of while
+        } catch (SQLException ex) {
+            while (ex != null) {
+                System.out.println("SQLState:  " + ex.getSQLState());
+                System.out.println("Error Code:" + ex.getErrorCode());
+                System.out.println("Message:   " + ex.getMessage());
+                Throwable t = ex.getCause();
+                while (t != null) {
+                    System.out.println("Cause:" + t);
+                    t = t.getCause();
+                }
+                ex = ex.getNextException();
+            }
+        }
+    }
+}
