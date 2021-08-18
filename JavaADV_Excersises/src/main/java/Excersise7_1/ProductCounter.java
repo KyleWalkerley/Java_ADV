18f6cd8b3a9d16b5bc54d0a6258a15c6fe6d76e0
KyleWalkerley <walkerleykyle@gmail.com>
2021-08-18 09:13:42 +0200

    17/08/21

--- JavaADV_Excersises/src/main/java/Excersise7_1/ProductCounter.java
+++ JavaADV_Excersises/src/main/java/Excersise7_1/ProductCounter.java
@@ -23,9 +23,9 @@ public class ProductCounter {
 
         Map<String, String> productNames = new TreeMap<>();
         productNames.put("Blue Polo Shirt", "1S01");
-        productNames.put("Black Polo Shirt", "1S02");
-        productNames.put("Red Ball Cap", "1H01");
-        productNames.put("Duke Mug   ", "1M02");
+        productNames.put("Red Polo Shirt", "1S02");
+        productNames.put("Green Ball Cap", "1H01");
+        productNames.put("Duke Muse   ", "1M02");
 
         ProductCounter pc1 = new ProductCounter(productNames);
         pc1.processList(parts);
